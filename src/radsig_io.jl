36c2acd132682aac4ac4d99299d0512418ba1572
Oliver Schulz <oschulz@mpp.mpg.de>
2019-05-09 15:23:20 +0200

    Rename :value to :values for waveform I/O

--- src/radsig_io.jl
+++ src/radsig_io.jl
@@ -5,20 +5,20 @@ function to_table(x::AbstractVector{<:RDWaveform})
     TypedTables.Table(
         t0 = first.(x.t),
         dt = step.(x.t),
-        value = x.v
+        values = x.v
     )
 end
 
 
-function _dtt02range(dt::RealQuantity, t0::RealQuantity, value::AbstractVector)
+function _dtt02range(dt::RealQuantity, t0::RealQuantity, values::AbstractVector)
     # TODO: Handle different units for dt and t0
-    t0 .+ (0:(size(value, 1) - 1)) .* dt
+    t0 .+ (0:(size(values, 1) - 1)) .* dt
 end
 
 function from_table(tbl, ::Type{<:AbstractVector{<:RDWaveform}})
     StructArray{RDWaveform}((
-        tbl.value,
-        _dtt02range.(tbl.dt, tbl.t0, tbl.value)
+        tbl.values,
+        _dtt02range.(tbl.dt, tbl.t0, tbl.values)
     ))
 end
 
@@ -37,7 +37,7 @@ function LegendDataTypes.readdata(
     input::HDF5.DataFile, name::AbstractString,
     AT::Type{<:AbstractVector{<:RDWaveform}}
 )
-    tbl = readdata(input, name, TypedTables.Table{<:NamedTuple{(:t0, :dt, :value)}})
+    tbl = readdata(input, name, TypedTables.Table{<:NamedTuple{(:t0, :dt, :values)}})
     from_table(tbl, AbstractVector{<:RDWaveform})
 end
 
