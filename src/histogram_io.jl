124d9ccc24913200cda6d523e65daefe4e9533d8
Lukas Hauertmann <lhauert@mpp.mpg.de>
2020-03-03 12:46:10 +0100

    Refactor conversion of Histograms to Namedtuples
    Add conversion of Ranges to NamedTuples

--- src/histogram_io.jl
+++ src/histogram_io.jl
@@ -1,16 +1,56 @@
-_nt_to_histogram(nt::NamedTuple) =
-    StatsBase.Histogram(values(nt.edges), nt.weights, nt.closed, nt.isdensity)  
+_range_to_nt(r::AbstractRange) = (
+    start = first(r),
+    stop = last(r),
+    step = step(r),
+    length = length(r)
+)
+_nt_to_range(nt::NamedTuple) =
+    range(nt.start, nt.stop, length = nt.length)
     
-function _histogram_to_nt(h::StatsBase.Histogram) 
-    n = ndims(h.weights)
-    edges_vec = collect.(h.edges)
-    edges_sym = Symbol.(["dim$(i)" for i in 1:n])
-    nt_edges = NamedTuple{tuple(edges_sym...)}(edges_vec)
+_range_to_nt(r::UnitRange) = (
+    start = first(r),
+    stop = last(r)
+)
+_nt_to_range(nt::NamedTuple{(:start, :stop)}) =
+    UnitRange(nt.start, nt.stop)
+
+_range_to_nt(r::StepRange) = (
+    start = first(r),
+    step = step(r),
+    stop = last(r)
+)
+_nt_to_range(nt::NamedTuple{(:start, :step, :stop)}) =
+    StepRange(nt.start, nt.step, nt.stop)
+
+_range_to_nt(r::LinRange) = (
+    start = first(r),
+    stop = last(r),
+    length = length(r)
+)
+_nt_to_range(nt::NamedTuple{(:start, :stop, :length)}) =
+    LinRange(nt.start, nt.stop, nt.length)
+
+
+function _nt_to_histogram(nt::NamedTuple)
+    return StatsBase.Histogram(
+        tuple(map(b ->_nt_to_range(b.binedges), nt.binning)...), 
+        nt.weights, 
+        nt.binning[1].closedleft ? :left : :right, 
+        nt.isdensity
+    )  
+end
+    
+function _histogram_to_nt(h::StatsBase.Histogram)
+    n::Int = ndims(h.weights)
+    axs_sym = Symbol.(["axis_$(i)" for i in Base.OneTo(n)])
+    axs = [(
+        binedges = _range_to_nt(h.edges[i]),
+        closedleft = h.closed == :left 
+    ) for i in Base.OneTo(n)]
     return (
-        n = ndims(h.weights),
-        ndims = 3,
+        ndims = n,
+        binning = NamedTuple{tuple(axs_sym...)}(axs),
         weights = h.weights,
-        edges = nt_edges,
         isdensity = h.isdensity,
         closed = h.closed    
     )
